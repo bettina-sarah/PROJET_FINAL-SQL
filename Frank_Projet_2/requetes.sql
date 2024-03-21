4ee3c33d725decbc1ac055b6454a7fd1bc988682
Bouchard François <e.fbouchard@etu.cvm.qc.ca>
2024-03-21 15:43:16 -0400

    Ajout d'inspection de troncon

---
+++ Frank_Projet_2/requetes.sql
@@ -0,0 +1,6 @@
+-- 5. Pour une inspection donnée, combien de kilomètres ont été parcourus.
+
+
+-- 6. Pour chacune des inspections, on désire savoir quels ont été les frais associés (vous devez tenir
+-- compte du temps passé pour les deux employés lors de l’inspection, des coûts d’exploitation du
+-- véhicule à 1.55$ par kilomètre). 
