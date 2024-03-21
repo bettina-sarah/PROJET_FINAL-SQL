b7065dc844effbc41c6c6633b4934a36feeabec0
Bouchard François <e.fbouchard@etu.cvm.qc.ca>
2024-03-21 16:05:36 -0400

    Test requete 5

--- Frank_Projet_2/requetes.sql
+++ Frank_Projet_2/requetes.sql
@@ -1,6 +1,49 @@
--- 5. Pour une inspection donnée, combien de kilomètres ont été parcourus.
+-- 
+-- =======================================================
+-- Requête #5
+-- Objectif : Pour une inspection donnée, combien de kilomètres ont été parcourus.
+-- Évaluation : ...
+-- Réalisé par : Francois Bouchard
+-- ...
+-- =======================================================
+SELECT inspection_id AS "Inspection", SUM(kilometrage_fin - kilometrage_debut) AS "Total KM"
+FROM Inspection_vehicule
+WHERE inspection_id = 1
+GROUP BY inspection_id
+
+SELECT 
+lit.troncon_id AS "Troncon",
+lit.voie AS "Voie",
+tr.longueur AS "Longueur"
+	FROM liste_inspection_troncon AS "lit"
+	INNER JOIN troncon AS "tr" 
+		ON tr.id = lit.troncon_id
+			WHERE inspection_id = 1
+
+-- =======================================================
 
 
--- 6. Pour chacune des inspections, on désire savoir quels ont été les frais associés (vous devez tenir
+
+-- =======================================================
+-- Requête #6
+-- Objectif : Pour chacune des inspections, on désire savoir quels ont été les frais associés (vous devez tenir
 -- compte du temps passé pour les deux employés lors de l’inspection, des coûts d’exploitation du
 -- véhicule à 1.55$ par kilomètre). 
+-- Évaluation : ...
+-- Réalisé par : Francois Bouchard
+-- ...
+-- =======================================================
+
+-- =======================================================
+
+
+
+-- =======================================================
+-- Requête #7
+-- Objectif : 
+-- Évaluation : ...
+-- Réalisé par : Francois Bouchard
+-- ...
+-- =======================================================
+
+-- =======================================================
